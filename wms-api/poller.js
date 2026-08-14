9e9ec6f88d209ee9c1cd72f78cff4ff0812f5a65
Dhiefferton <compras@boxersoldas.com.br>
2026-08-14 13:03:59 -0300

    Export executarCiclo from poller.js

--- wms-api/poller.js
+++ wms-api/poller.js
@@ -202,4 +202,4 @@ function iniciarPollingZenErp() {
     setInterval(executarCiclo, POLL_INTERVAL_MS);
 }
 
-module.exports = { iniciarPollingZenErp, zenErpGet, zenErpPost };
+module.exports = { iniciarPollingZenErp, zenErpGet, zenErpPost, executarCiclo };
