843fa7128fe5922faafce1172d88bcab07b20f81
Dhiefferton <compras@boxersoldas.com.br>
2026-08-16 09:03:58 -0300

    Add SeparacaoErp route to App component

--- wms-coletor/src/App.jsx
+++ wms-coletor/src/App.jsx
@@ -1,21 +1,23 @@
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import Menu from './pages/Menu.jsx';
 import Separacao from './pages/Separacao.jsx';
+import SeparacaoErp from './pages/SeparacaoErp.jsx';
 import Reposicao from './pages/Reposicao.jsx';
 import Inventario from './pages/Inventario.jsx';
 import Picking from './pages/Picking.jsx';
 import NfImportacao from './pages/NfImportacao.jsx';
 export default function App() {
-    return (
-        <HashRouter>
-            <Routes>
-                <Route path="/" element={<Menu />} />
-                <Route path="/separacao" element={<Separacao />} />
-                <Route path="/reposicao" element={<Reposicao />} />
-                <Route path="/inventario" element={<Inventario />} />
-                <Route path="/picking" element={<Picking />} />
-                <Route path="/nf-importacao" element={<NfImportacao />} />
-            </Routes>
-        </HashRouter>
-    );
+return (
+<HashRouter>
+<Routes>
+<Route path="/" element={<Menu />} />
+<Route path="/separacao" element={<Separacao />} />
+<Route path="/separacao-erp" element={<SeparacaoErp />} />
+<Route path="/reposicao" element={<Reposicao />} />
+<Route path="/inventario" element={<Inventario />} />
+<Route path="/picking" element={<Picking />} />
+<Route path="/nf-importacao" element={<NfImportacao />} />
+</Routes>
+</HashRouter>
+);
 }
