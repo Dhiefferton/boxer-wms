73813c3e30bf73e923e78cff7048b7dd2b053677
Dhiefferton <compras@boxersoldas.com.br>
2026-08-28 07:45:15 -0300

    Add route for ConferenciaErp page

--- wms-coletor/src/App.jsx
+++ wms-coletor/src/App.jsx
@@ -6,6 +6,7 @@ import Reposicao from './pages/Reposicao.jsx';
 import Inventario from './pages/Inventario.jsx';
 import Picking from './pages/Picking.jsx';
 import NfImportacao from './pages/NfImportacao.jsx';
+import ConferenciaErp from './pages/ConferenciaErp.jsx';
 export default function App() {
 return (
 <HashRouter>
@@ -17,6 +18,7 @@ return (
 <Route path="/inventario" element={<Inventario />} />
 <Route path="/picking" element={<Picking />} />
 <Route path="/nf-importacao" element={<NfImportacao />} />
+<Route path="/conferencia-erp" element={<ConferenciaErp />} />
 </Routes>
 </HashRouter>
 );
