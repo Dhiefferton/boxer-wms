5caac2bfbb582c261b462bf173ea798cdc40c494
Dhiefferton Lamin <compras@boxersoldas.com.br>
2026-09-01 14:40:26 -0300

    chore: remove rotas de debug temporarias e limpa index.js

--- wms-api/index.js
+++ wms-api/index.js
@@ -7,63 +7,55 @@ const recebimentoRouter = require('./routes/recebimento');
 const produtosRouter = require('./routes/produtos');
 const pedidosRouter = require('./routes/pedidos');
 const inventarioRouter = require('./routes/inventario');
+const reconciliarErpRouter = require('./routes/reconciliar-erp');
 const areasFlutuanteRouter = require('./routes/areas-flutuante');
 const cadastroEnderecosRouter = require('./routes/cadastro-enderecos');
 const unidadesSerializadasRouter = require('./routes/unidades-serializadas');
 const movimentacoesRouter = require('./routes/movimentacoes');
 const nfImportacaoRouter = require('./routes/nf-importacao');
 const pickingRouter = require('./routes/picking');
-const debugRouter = require('./routes/debug');
-const debug2Router = require('./routes/debug2');
 const erpCronRouter = require('./routes/erp-cron');
 const separacaoErpRouter = require('./routes/separacao-erp');
-const debug4Router = require('./routes/debug4');
 const backfillPerfilRouter = require('./routes/backfill-perfil');
+const conferenciaErpRouter = require('./routes/conferencia-erp');
+const historicoRouter = require('./routes/historico');
 const { iniciarPollingZenErp } = require('./poller');
 const { iniciarAgendaInventario } = require('./agenda-inventario');
-const debug5Router = require('./routes/debug5');
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 app.use('/enderecos', enderecosRouter);
 app.use('/tarefas', tarefasRouter);
 app.use('/recebimento', recebimentoRouter);
 app.use('/produtos', produtosRouter);
 app.use('/pedidos', pedidosRouter);
 app.use('/inventario', inventarioRouter);
-const reconciliarErpRouter = require('./routes/reconciliar-erp');
+app.use('/reconciliar', reconciliarErpRouter);
 app.use('/areas-flutuante', areasFlutuanteRouter);
 app.use('/cadastro-enderecos', cadastroEnderecosRouter);
-const debug3Router = require('./routes/debug3');
 app.use('/unidades-serializadas', unidadesSerializadasRouter);
 app.use('/movimentacoes', movimentacoesRouter);
 app.use('/nf-importacao', nfImportacaoRouter);
 app.use('/picking', pickingRouter);
-app.use('/debug', debugRouter);
 app.use('/erp', erpCronRouter);
 app.use('/separacao-erp', separacaoErpRouter);
-app.use('/debug2', debug2Router);
-app.use('/debug3', debug3Router);
-app.use('/debug4', debug4Router);
 app.use('/backfill', backfillPerfilRouter);
-app.use('/debug5', debug5Router);
-const debug6Router = require('./routes/debug6');
-const debug7Router = require('./routes/debug7'); const conferenciaErpRouter = require('./routes/conferencia-erp'); const historicoRouter = require('./routes/historico');
-app.use('/reconciliar', reconciliarErpRouter);
-app.use('/debug6', debug6Router);
 app.use('/conferencia-erp', conferenciaErpRouter);
 app.use('/historico', historicoRouter);
-app.use('/debug7', debug7Router);
+
 app.get('/', (req, res) => {
     res.json({ status: 'ok', servico: 'WMS API' });
 });
+
 if (require.main === module) {
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
         console.log(`WMS API rodando na porta ${PORT}`);
         iniciarPollingZenErp();
-
         iniciarAgendaInventario();
     });
 }
+
 module.exports = app;
